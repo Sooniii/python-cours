710096e1a17444f6418ea75010b9238461dcf0c9
Théodule <theodule.janchen@edu.itescia.fr>
2020-10-26 11:22:21 +0100

    Premier commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from art import *
+
+def say_hello(name):
+    print(f"Hello {name} ")
+
+my_name = "Théodule"
+say_hello(my_name)
+
+h = art("Hello")
+print(h)
+print(text2art("13 Block", font="block"))
