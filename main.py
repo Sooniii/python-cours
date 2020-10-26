712f2f2758991f94329672e900767a843803037e
Théodule <theodule.janchen@edu.itescia.fr>
2020-10-26 15:24:09 +0100

    Debut du TP exercice

--- main.py
+++ main.py
@@ -1,11 +1,39 @@
-from art import *
+repertoire = {}
+program = True
 
-def say_hello(name):
-    print(f"Hello {name} ")
 
-my_name = "Théodule"
-say_hello(my_name)
+def create_contact():
+    contact = {
+        "name": input("Prénom : "),
+        "phone_number": input("Numéro : "),
+        "is_favorite": input("Favori (True or False) : "),
+    }
+    return contact
 
-h = art("Hello")
-print(h)
-print(text2art("GITHUB", font="block"))
+
+def add_contact(rep, contact):
+    number = contact["phone_number"]
+    rep[number] = contact
+
+
+def get_names(rep):
+    name_list = []
+    for k in rep:
+        contact = rep[k]
+        name_list.append(contact["name"])
+    name_list.sort()
+    print(name_list)
+
+
+while program:
+
+    new_contact = create_contact()
+    add_contact(repertoire, new_contact)
+    next_name = input("Rentré un nouveau numéro ? ")
+    if next_name == "Non":
+        program = False
+
+get_names(repertoire)
+
+
+
